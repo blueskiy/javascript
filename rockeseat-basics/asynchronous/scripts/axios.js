fbef0047505986697a4887cd9407bebf1f37df92
Filipe André <filipeandres@hotmail.com>
2020-10-20 08:39:01 -0300

    start in axios

---
+++ rockeseat-basics/asynchronous/scripts/axios.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+
+axios.get('https://api.github.com/users/blueskiy')
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  })
