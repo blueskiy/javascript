7149ca6a529f714964b919b05963158f38df741f
Filipe André <filipeandres@hotmail.com>
2021-02-02 08:54:54 -0300

    Throw / Try-Catch

--- javascript-bootcamp/advanced/try-catch.js
+++ javascript-bootcamp/advanced/try-catch.js
@@ -1,6 +1,14 @@
 const getTip = (amount) => {
-    return amount * .25
+    if(typeof amount === 'number') {
+        return amount * .25
+    } else {
+        throw Error('Argument must be a number')
+    }
 }
 
-const tip = getTip(42)
-console.log(tip);
+try {
+    const reult = getTip(10)
+    console.log(reult);
+} catch (error) {
+    console.log('catch block is runing');
+}
