fb27fa00654bc177d8b8a0dde7d94af6f638afad
Filipe André <filipeandres@hotmail.com>
2021-02-05 07:48:22 -0300

    Hangman

--- javascript-bootcamp/oop/hangman.js
+++ javascript-bootcamp/oop/hangman.js
@@ -1,10 +1,25 @@
 const Hangman = function(word, remainingGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
+}
+
+Hangman.prototype.getPuzzle = function() {
+    let puzzle = ''
+
+    this.word.forEach(letter => {
+        if(this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    });
+
+    return puzzle
 }
 
 const instanceOne = new Hangman('cat', 10)
-const instanceTwo = new Hangman('butterfly', 10)
+const instanceTwo = new Hangman('Samba Canção', 10)
 
-console.log(instanceOne);
-console.log(instanceTwo);
+console.log(instanceOne.getPuzzle());
+console.log(instanceTwo.getPuzzle());
